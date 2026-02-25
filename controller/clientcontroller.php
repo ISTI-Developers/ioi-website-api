6f7a121fe69a43c0c24706efba5e5e800c0c60d3
davelopez. <mrdavelopez28@gmail.com>
2026-02-25 10:38:07 +0800

    Reapply "client controller"
    This reverts commit a6f197ff4b98eb5ab96f628ac9c62ca5245a9218.

---
+++ controller/clientcontroller.php
@@ -0,0 +1,82 @@
+<?php
+
+
+require_once 'controller.php';
+
+
+
+class ClientController extends Controller {
+
+    public function get() 
+    {
+        $data = $this->getRecords(
+            "ioi_clients",
+            [],
+            [],
+            "many",
+            "*",
+            "ORDER BY client_id ASC"
+
+        );
+        $this->send($data);
+    }
+
+
+    public function getOne() 
+    {
+        $data = $this->getRecords(
+            "ioi_clients",
+            ["role_id"],
+            [$id],
+            "one"
+        );
+
+        $this->send($data);
+    }
+
+
+
+    public function add()
+    {
+        $data = json_decode($_POST['data'], true);
+        extract($data);
+
+        $filePath = null;
+        if(!empty($_FILES['file']['name'][0]) && $_FILES['file']['error'][0] === UPLOAD_ERR_OK) {
+        $uploadDir = __DIR__ . "/uploads/";
+        if(!is_dir($uploadDir)) mkdir($uploadDir, 0777, true);
+
+        $fileName = time() . "_" . basename($_FILES['file']['name'][0]);
+        $filePath = "uploads/" . $fileName;
+
+
+        move_uploaded_file($_FILES['file']['tmp_name'][0], $uploadDir . $fileName);
+        }
+
+        if(empty($client_name)) $this->send(["message" => "Client Name is required"], 400);
+        if(empty($client_description)) $this->send(["message" => "Client Description is required"], 400);
+
+
+        $client_id = $this->addRecords(
+            "ioi_clients",
+            ["client_name", "client_description", "file"],
+            [
+                $client_name,
+                $client_description,
+                $filePath ?? ""
+            ]
+        );
+
+        if(!$client_id) {
+            var_dump($this->db->errorInfo());
+            die("Insert failed");
+        }
+
+        return $this->send([
+            "message" => "Client created successfully",
+            "id" => $client_id
+        ]);
+    }
+
+}
+
