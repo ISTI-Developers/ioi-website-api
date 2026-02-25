a6f197ff4b98eb5ab96f628ac9c62ca5245a9218
davelopez. <mrdavelopez28@gmail.com>
2026-02-25 10:37:32 +0800

    Revert "client controller"
    This reverts commit 2fc4dbcc7d1f4b6ab70818bfe42b97bf99aac0be.

--- index.php
+++ index.php
@@ -13,13 +13,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 
 require __DIR__ . '/controller/teamcontroller.php';
 require __DIR__ . '/controller/rolescontroller.php';
-require __DIR__ . '/controller/clientcontroller.php';
 
 
 $routes = [
     "team" => new TeamController(),
     "roles" => new RolesController(),
-    "clients" => new ClientController(),
 
 ];
 
